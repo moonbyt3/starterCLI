cb85d81621c1a58c345462bc6cbcae7c38819100
Milorad <miloradjekic@outlook.com>
2019-06-11 18:38:49 +0200

    fixed gen-p command

--- cli-page.js
+++ cli-page.js
@@ -18,16 +18,26 @@ fs.readFile(`__fe-templates/fe-page-example.php`, 'utf8', function(err, contents
         let fileContent = contents.split('\n');
         let positionsOfString = [];
         // finds @ package NAMEOFTHEME
-        for (var i = 0; i < fileContent.length; i++) {
+        for (let i = 0; i < fileContent.length; i++) {
             let package = fileContent[i].search(' * @package');
             
             if (package > 0) {
                 positionsOfString.push(i);
             }
         }
-        let packageLine = fileContent[positionsOfString].split(' ');
+        let packageLine = fileContent[positionsOfString[0]].split(' ');
+        let templateNameLine = fileContent[2].split('-');
+        let templateName = pageName.split("-");
+        let newTemplateNameArr = [];
+        for (let i = 0; i < templateName.length; i++) {
+            templateName[i] = capitalize(templateName[i]);
+            newTemplateNameArr.push(templateName[i]);
+        }
+        let newTemplateName = newTemplateNameArr.join(" ");
+        fileContent.splice(2, 1, ` * Template Name: FE Dev - ${newTemplateName}`);
+        fileContent.splice(6, 1, ` * @package ${packageLine[3]}`);
+       
         
-        fileContent.splice(fileContent[positionsOfString], 0, `${packageLine[3]}`);
         let str = fileContent.join('\n');
         fs.appendFile(`__fe-templates/fe-page-${pageName}.php`, str, function(err) {
             if (err) throw err;
@@ -37,3 +47,8 @@ fs.readFile(`__fe-templates/fe-page-example.php`, 'utf8', function(err, contents
         console.log(`ERR: fe-page-example.php doesn't exist, check if you have it in the right directory.`);
     }
 });
+
+const capitalize = (s) => {
+  if (typeof s !== 'string') return ''
+  return s.charAt(0).toUpperCase() + s.slice(1)
+}
